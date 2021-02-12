5f474ad3562f207d53aaf5c50df4d3937d903c20
Petr <superdytko@gmail.com>
2021-02-12 18:01:10 +0300

    final commit

--- homework.py
+++ homework.py
@@ -14,7 +14,7 @@ PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 BASE_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 MESSAGE_DATA = {
     'rejected': 'К сожалению в работе нашлись ошибки.',
     'reviewing': 'работа взята в ревью',
@@ -34,29 +34,30 @@ def parse_homework_status(homework):
 
 def get_homework_statuses(current_timestamp):
     params = {'from_date': current_timestamp}
-    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    headers = HEADERS
     if current_timestamp is not None:
         try:
-            homework_statuses = requests.get(
+            response = requests.get(
                 BASE_URL, headers=headers, params=params)
-        except requests.exceptions.HTTPError as error:
-            logging.exception(error)
         except requests.RequestException as er:
-            logging.exception(er)
+            raise Exception(f'ошибка: {er}')
         else:
-            return homework_statuses.json()
+            if response.json().get('error'):
+                raise Exception(f'ошибка: ')
+            return response.json()
+    else:
+        print('ошибка данных')
 
 
 def send_message(message, bot_client):
-    logging.basicConfig(filename='main.log', filemode='w')
-    logging.info('Отправлено сообщение в чат Telegram')
     return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = 0  # int(time.time())
-
+    logging.debug('Я бот и я запустился')
+    current_timestamp = int(time.time())
+    current_timestamp = int(0)
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
@@ -66,13 +67,14 @@ def main():
                     bot_client)
             current_timestamp = new_homework.get('current_date',
                                                  current_timestamp)
+            logging.info('Отправлено сообщение в чат Telegram')
             time.sleep(TIME_SLEEP1)
 
-        except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
-            send_message(e, bot_client)
+        except requests.RequestException as e:
+            logging.debug(f'Бот столкнулся с ошибкой: {e}')
             time.sleep(TIME_SLEEP)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='main.log', filemode='w')
     main()
